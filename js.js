d49b8ef34951dc27b5802bd9bc025658f6756b07
Lucas Rogers <S530415@nwmissouri.edu>
2020-03-06 15:41:24 -0600

    Initial Upload

---
+++ js.js
@@ -0,0 +1,41 @@
+const inputName = document.querySelector("#fname")
+const inputAct = document.querySelector("#act")
+const inputHours = document.querySelector("#cost")
+const inputPercent = document.querySelector("#percent")
+const btnCombine = document.getElementById("combine")
+const mergeHold = document.getElementById("merg")
+const theTip = document.getElementById("tip")
+
+const newsURL ='https://newsapiraygorodskijv1.p.rapidapi.com/getArticles'
+window.addEventListener('load', (event) => {
+    document.querySelector("#fname").innerHTML = localStorage.getItem('name'); 
+})
+const tip = function (numcost, percent)
+{
+    console.log(numcost, percent)
+    if(cost<0 || wage <0){
+    return 0;
+    }
+    else{
+    return (numcost * percent);
+    }
+    
+}
+
+btnCombine.onclick = function(){
+    const strcost = inputcost.value;
+    const numcost = parseFloat(strcost);
+    const strpercent = inputPercent.value;
+    const numpercent = parseFloat(strpercent);
+    const name = inputName.value;
+    const act = inputAct.value;
+    mergeHold.innerHTML = name + " you love to "+ act;
+    localStorage.setItem('name',name)
+    const tiptotal = tip(numcost, numpercent)
+    if (tiptotal == 0){
+        payHold.innerHTML = name + " recheck your inputs please.";
+    }
+    else{
+        theTip.innerHTML = name + " your tip should be "+ tiptotal;
+    }
+}
